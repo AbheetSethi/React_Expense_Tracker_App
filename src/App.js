75e0cd793c13b26506863a8396ad532a4b89cdc3
Abheet Sethi <abheeet.sethi@gmail.com>
2021-08-31 02:36:33 +0530

    Expense Tracker App
    This is my first React app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Header } from './components/Header.js';
+import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
+import { AddTransaction } from './components/AddTransaction.js';
+import { GlobalProvider } from './context/GlobalState.js';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
